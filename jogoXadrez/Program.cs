932cf3d56ff4d0df3226303c97456fe705f30388
Paulo Maggio <paullomagio@gmail.com>
2025-11-14 13:45:44 -0300

    Mostrando peças capturadas

--- jogoXadrez/Program.cs
+++ jogoXadrez/Program.cs
@@ -19,10 +19,7 @@ namespace jogoXadrez
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Digite a posição de origem: ");
